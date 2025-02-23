5e8493012242a14ce876808a33e2b8d97ceee96d
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-02-24 02:08:56 +0530

    Completed till video 44

---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+// https://mongoosejs.com/docs/guide.html
+
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    tag:{
+        type: String,
+        default: "General"
+    },
+    timestamp:{
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('notes', NotesSchema)
