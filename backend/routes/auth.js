d7282749cc225e345e731bcc19d3956413da0660
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-03-09 18:43:22 +0530

    Completed till video 49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
+
+const JWT_SECRET = 'Harryisagoodb$oy'
 
 // Create a user using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -21,15 +25,25 @@ router.post('/createuser', [
         if(user){
             return res.status(400).json({error: "Sorry a user with this email already exist"})
         }
-        
+        const salt = await bcrypt.genSalt(10)
+        const secPass = await bcrypt.hash(req.body.password, salt)
         // Create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
         
-        res.json(user)
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        // console.log(authtoken)
+
+        // res.json(user)
+        res.json({authtoken})
         
     } catch (error) {
         console.error(error.message)
@@ -45,3 +59,5 @@ module.exports = router
 
 
 // https://express-validator.github.io/docs/6.12.0/
+// https://jwt.io/
+// https://www.npmjs.com/package/jsonwebtoken
