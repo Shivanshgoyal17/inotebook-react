e201fdad489c72951b283164d88185ff0db4986e
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-03-23 01:22:48 +0530

    Completed till video 62

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -11,7 +11,52 @@ const NoteState = (props) =>{
           "tag": "personal",
           "date": "2025-03-12T09:20:12.479Z",
           "__v": 0
-        }
+        },
+        {
+            "_id": "67d151cc4ea0fd2dfcbb9bf4",
+            "user": "67ce0ff43af267b147d26200",
+            "title": "My Title2",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2025-03-12T09:20:12.479Z",
+            "__v": 0
+          },
+          {
+            "_id": "67d151cc4ea0fd2dfcbb9bf5",
+            "user": "67ce0ff43af267b147d26200",
+            "title": "My Title3",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2025-03-12T09:20:12.479Z",
+            "__v": 0
+          },
+          {
+            "_id": "67d151cc4ea0fd2dfcbb9bf6",
+            "user": "67ce0ff43af267b147d26200",
+            "title": "My Title4",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2025-03-12T09:20:12.479Z",
+            "__v": 0
+          },
+          {
+            "_id": "67d151cc4ea0fd2dfcbb9bf7",
+            "user": "67ce0ff43af267b147d26200",
+            "title": "My Title5",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2025-03-12T09:20:12.479Z",
+            "__v": 0
+          },
+          {
+            "_id": "67d151cc4ea0fd2dfcbb9bf8",
+            "user": "67ce0ff43af267b147d26200",
+            "title": "My Title6",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2025-03-12T09:20:12.479Z",
+            "__v": 0
+          }
       ]
 
       const [notes, setNotes] = useState(notesInitial)
