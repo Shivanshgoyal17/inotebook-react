99d5f993f2dc1b7b0e11dbfc7a749b54060244d8
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-03-23 15:48:47 +0530

    Completed till video 63

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,58 +13,84 @@ const NoteState = (props) =>{
           "__v": 0
         },
         {
-            "_id": "67d151cc4ea0fd2dfcbb9bf4",
-            "user": "67ce0ff43af267b147d26200",
-            "title": "My Title2",
-            "description": "My description",
-            "tag": "personal",
-            "date": "2025-03-12T09:20:12.479Z",
-            "__v": 0
-          },
-          {
-            "_id": "67d151cc4ea0fd2dfcbb9bf5",
-            "user": "67ce0ff43af267b147d26200",
-            "title": "My Title3",
-            "description": "My description",
-            "tag": "personal",
-            "date": "2025-03-12T09:20:12.479Z",
-            "__v": 0
-          },
-          {
-            "_id": "67d151cc4ea0fd2dfcbb9bf6",
-            "user": "67ce0ff43af267b147d26200",
-            "title": "My Title4",
-            "description": "My description",
-            "tag": "personal",
-            "date": "2025-03-12T09:20:12.479Z",
-            "__v": 0
-          },
-          {
-            "_id": "67d151cc4ea0fd2dfcbb9bf7",
-            "user": "67ce0ff43af267b147d26200",
-            "title": "My Title5",
-            "description": "My description",
-            "tag": "personal",
-            "date": "2025-03-12T09:20:12.479Z",
-            "__v": 0
-          },
-          {
-            "_id": "67d151cc4ea0fd2dfcbb9bf8",
-            "user": "67ce0ff43af267b147d26200",
-            "title": "My Title6",
-            "description": "My description",
-            "tag": "personal",
-            "date": "2025-03-12T09:20:12.479Z",
-            "__v": 0
-          }
+          "_id": "67d151cc4ea0fd2dfcbb9bf4",
+          "user": "67ce0ff43af267b147d26200",
+          "title": "My Title2",
+          "description": "My description",
+          "tag": "personal",
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        },
+        {
+          "_id": "67d151cc4ea0fd2dfcbb9bf5",
+          "user": "67ce0ff43af267b147d26200",
+          "title": "My Title3",
+          "description": "My description",
+          "tag": "personal",
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        },
+        {
+          "_id": "67d151cc4ea0fd2dfcbb9bf6",
+          "user": "67ce0ff43af267b147d26200",
+          "title": "My Title4",
+          "description": "My description",
+          "tag": "personal",
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        },
+        {
+          "_id": "67d151cc4ea0fd2dfcbb9bf7",
+          "user": "67ce0ff43af267b147d26200",
+          "title": "My Title5",
+          "description": "My description",
+          "tag": "personal",
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        },
+        {
+          "_id": "67d151cc4ea0fd2dfcbb9bf8",
+          "user": "67ce0ff43af267b147d26200",
+          "title": "My Title6",
+          "description": "My description",
+          "tag": "personal",
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        }
       ]
 
       const [notes, setNotes] = useState(notesInitial)
-        
-    return(
-        <noteContext.Provider value={{notes, setNotes}}>
-            {props.children}
-        </noteContext.Provider>
+
+      // Add a note
+      const addNote = (title, description, tag) =>{
+        // TODO: API call
+        console.log("Adding a new note")
+        const note = {
+          "_id": "67d151cc4ea0fd2dfcbb9bf3",
+          "user": "67ce0ff43af267b147d26200",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2025-03-12T09:20:12.479Z",
+          "__v": 0
+        }
+        setNotes(notes.concat(note))
+      }
+
+      // Delete a note
+      const deleteNote = () =>{
+
+      }
+
+      // Edit a note
+      const editNote = () =>{
+
+      }
+
+      return(
+          <noteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
+              {props.children}
+          </noteContext.Provider>
 
     )
 }
