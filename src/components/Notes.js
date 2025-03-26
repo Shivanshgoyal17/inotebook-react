1a2783259dfaef71a669bba5610abe1301c1903c
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-03-27 00:40:11 +0530

    Completed till video 67

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,23 +5,25 @@ import AddNote from "./AddNote"
 
 const Notes = () => {
   const context = useContext(noteContext)
-  const { notes, getNotes } = context
+  const { notes, getNotes, editNote } = context
   useEffect(() => {
     getNotes()
     // eslint-disable-next-line
   }, [])
   
   const ref = useRef(null)
-  const [note, setNote] = useState({etitle: "", edescription: "", etag: ""})
+  const refClose = useRef(null)
+  const [note, setNote] = useState({id: "", etitle: "", edescription: "", etag: ""})
 
   const updateNote = (currentNote) => {
     ref.current.click()
-    setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag})
+    setNote({id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag})
   }
 
   const handleClick = (e) => {
     console.log("Updating the note...", note)
-    e.preventDefault()
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refClose.current.click()
   }
 
   const onChange = (e) => {
@@ -31,7 +33,7 @@ const Notes = () => {
   return (
     <>
       <AddNote />
-      <button ref={ref} type="button" class="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
         Launch demo modal
       </button>
 
@@ -67,7 +69,7 @@ const Notes = () => {
               </form>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+              <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">
                 Close
               </button>
               <button onClick={handleClick} type="button" className="btn btn-primary">
