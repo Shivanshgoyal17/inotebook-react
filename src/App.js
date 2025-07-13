11ae5ef2206107753e96e362033c0f192faa996e
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-07-14 03:53:10 +0530

    Completed till video 69

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
     <NoteState>
       <Router>
         <Navbar />
-        <Alert />
+        <Alert message="This is Amazing React Course"/>
         <div className="container">
           <Routes>
-            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/" element={<Home/>}></Route>
             <Route exact path="/about" element={<About/>}></Route>
+            <Route exact path="/login" element={<Login/>}></Route>
+            <Route exact path="/signup" element={<Signup/>}></Route>
           </Routes>
         </div>
       </Router>
