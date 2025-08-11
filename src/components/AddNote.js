724d8e70c07a94dfa015dcd2186a9efdb5855dd1
Shivanshgoyal17 <shivanshgoyal2001@gmail.com>
2025-08-12 03:07:25 +0530

    Completed till Video 71

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,16 @@
 import React, { useState, useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 
-const AddNote = () => {
+const AddNote = (props) => {
   const context = useContext(noteContext);
   const {addNote} = context;
   
-  const [note, setNote] = useState({title: "", description: "", tag: "default"})
+  const [note, setNote] = useState({title: "", description: "", tag: ""})
   const handleClick = (e) =>{
     e.preventDefault()
     addNote(note.title, note.description, note.tag)
+    setNote({title: "", description: "", tag: ""})
+    props.showAlert("Added Successfully", "success")
   }
   const onChange = (e) =>{
     setNote({...note, [e.target.name]: e.target.value})
